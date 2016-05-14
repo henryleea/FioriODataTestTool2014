674acf8b8fb600972ebed2df4c48d868018c6320
i042416 <cle.ee@163.com>
2016-05-14 20:23:11 +0800

    write another bloig

--- WebContent/wenxin/sapui5/MyView.view.js
+++ WebContent/wenxin/sapui5/MyView.view.js
@@ -1,21 +1,15 @@
 sap.ui.jsview("wenxin.sapui5.MyView", {
 
-	/** Specifies the Controller belonging to this View. 
-	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-	* @memberOf sapui5.MyView
-	*/ 
 	getControllerName : function() {
 		return "wenxin.sapui5.MyView";
 	},
 
-	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
-	* Since the Controller is given to this method, its event handlers can be attached right away. 
-	* @memberOf sapui5.MyView
-	*/ 
 	createContent : function(oController) {
 		var myButton = new sap.ui.commons.Button("btn",{
-			text: "press me~",
-		    press:"onPress"
+			text: "press me~"
+		});
+		var myButton2 = new sap.ui.commons.Button("btn",{
+			text: "press me~"
 		});
 		myButton.attachPress(function(){
 			oController.onPress();
