f9564d5aada3665bec7f10ac9b6d4e6c76b42d7d
jerry <jerry.wang@sap.com>
2017-04-22 16:08:43 +0200

    blog done

--- WebContent/buttontutorial/view/simple.controller.js
+++ WebContent/buttontutorial/view/simple.controller.js
@@ -8,52 +8,30 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"], function(Controller){
 	"use strict";
 	return Controller.extend("buttontutorial.view.simple",{
-	onInit : function() {
-	},
 	
-	onBeforeRendering: function() {
-	},
-	
+	BIG_IMAGE: "https://cloud.githubusercontent.com/assets/5669954/25305089/3c4d7490-2775-11e7-9422-b5d830a59732.jpg",
 	onPress: function(){
 		var image = this.byId("jerryImage");
 		this.loadImageWithProxy(image);
 	},
 	
 	injectProxy: (function(){
-		var imgCache = new Image();
+		var imgProxy = new Image();
 
         return function(img, src){
-            imgCache.onload = function(){
-            	debugger;
-            	
+            imgProxy.onload = function(){
         		img.setSrc(this.src);
             };
-            // var oDom = img.getDomRef();
             img.setSrc("buttontutorial/view/loading.gif");
-            // oDom.src = "http://localhost:9098/2014-12-30-FioriODataTest/buttontutorial/view/loading.gif";
-            imgCache.src = src;
+            imgProxy.src = this.BIG_IMAGE;
         };
 	})(),
-	loadImageNormal: function(){
-		var image = this.byId("jerryImage");
-		image.setSrc("https://cloud.githubusercontent.com/assets/5669954/24836808/7d78976e-1d58-11e7-9c28-2c76d90c9e12.png");
-		debugger;
-	}, 
-	loadImageWithProxy: function(){
-		var image = this.byId("jerryImage");
-		this.injectProxy(image, "https://cloud.githubusercontent.com/assets/5669954/24836808/7d78976e-1d58-11e7-9c28-2c76d90c9e12.png");
-    },
-	onAfterRendering: function() {
-		
-		var oButton = this.getButtonReference();
-		var oDom = oButton.getDomRef();
-		jQuery.sap.require("sap.ui.core.theming.Parameters");
-		oDom.style.color = sap.ui.core.theming.Parameters.get("sapUiAccent2");
-		oDom.style.backgroundColor = sap.ui.core.theming.Parameters.get("sapUiErrorBG");
-	},
 	
-	getButtonReference: function() {
-		return this.getView().byId("jerryButton");
-	}
+	loadImageNormal: function(image){
+		image.setSrc(this.BIG_IMAGE);
+	}, 
+	loadImageWithProxy: function(image){
+		this.injectProxy(image);
+    }
   });}
 );
