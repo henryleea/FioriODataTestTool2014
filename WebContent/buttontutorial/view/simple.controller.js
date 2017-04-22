1dbbd31b42775faea80263be2de910f90fe78662
jerry <jerry.wang@sap.com>
2017-04-22 14:01:17 +0200

    barely works

--- WebContent/buttontutorial/view/simple.controller.js
+++ WebContent/buttontutorial/view/simple.controller.js
@@ -1,23 +1,50 @@
 /*
  * Jerry 2015-11-01 8:34AM 
  * http://scn.sap.com/community/fiori/blog/2015/10/28/how-i-do-self-study-on-a-given-fiori-control--part-9
- sap.ui.controller("buttontutorial.view.simple", { */
+ sap.ui.controller("buttontutorial.view.simple", { 
+ */
 
 
 sap.ui.define(["sap/ui/core/mvc/Controller"], function(Controller){
 	"use strict";
 	return Controller.extend("buttontutorial.view.simple",{
 	onInit : function() {
-		var oButton = this.getButtonReference();
-		var oDom = oButton.getDomRef();
 	},
 	
 	onBeforeRendering: function() {
-		var oButton = this.getButtonReference();
-		var oDom = oButton.getDomRef();
 	},
 	
+	onPress: function(){
+		var image = this.byId("jerryImage");
+		this.loadImageWithProxy(image);
+	},
+	
+	injectProxy: (function(){
+		var imgCache = new Image();
+
+        return function(img, src){
+            imgCache.onload = function(){
+            	debugger;
+            	
+        		img.setSrc(this.src);
+            };
+            // var oDom = img.getDomRef();
+            img.setSrc("buttontutorial/view/loading.gif");
+            // oDom.src = "http://localhost:9098/2014-12-30-FioriODataTest/buttontutorial/view/loading.gif";
+            imgCache.src = src;
+        };
+	})(),
+	loadImageNormal: function(){
+		var image = this.byId("jerryImage");
+		image.setSrc("https://cloud.githubusercontent.com/assets/5669954/24836808/7d78976e-1d58-11e7-9c28-2c76d90c9e12.png");
+		debugger;
+	}, 
+	loadImageWithProxy: function(){
+		var image = this.byId("jerryImage");
+		this.injectProxy(image, "https://cloud.githubusercontent.com/assets/5669954/24836808/7d78976e-1d58-11e7-9c28-2c76d90c9e12.png");
+    },
 	onAfterRendering: function() {
+		
 		var oButton = this.getButtonReference();
 		var oDom = oButton.getDomRef();
 		jQuery.sap.require("sap.ui.core.theming.Parameters");
@@ -28,6 +55,5 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function(Controller){
 	getButtonReference: function() {
 		return this.getView().byId("jerryButton");
 	}
-	
   });}
 );
